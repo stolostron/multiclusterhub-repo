77f9c52b7db981628c61ecf60ef1ff291b38e1b3
Connor Wyckoff <connor.wyckoff@ibm.com>
2020-02-12 21:57:18 -0500

    fix script and re-add charts

--- chart-sync.sh
+++ chart-sync.sh
@@ -2,19 +2,18 @@ package_chart()
 {
   cd stable/*/ 
   PACKAGE="$(helm package ./)"
-  find . -name '*tgz' | xargs -J% mv % ../../../
-  cd ../../../../../
+  find . -name '*tgz' | xargs -J% mv % ../../../multicloudhub/charts
+  cd ../../../
   echo "after" $PWD
 }
 echo "Fetching charts from SOURCE-REPO"
-mkdir -p cp4mcm-install/charts
+mkdir -p multicloudhub/charts
 git submodule update --init --recursive
 SUB_LIST="$(grep path .gitmodules | sed 's/.*= //')"
 for submodule in ${SUB_LIST}; do
   echo ${submodule}
-  mv ${submodule}  cp4mcm-install/charts
-  cd "cp4mcm-install/charts/${submodule}"
+  cd "${submodule}"
   package_chart 
-  rm -rf cp4mcm-install/charts/${submodule}
+  rm -rf multicloudhub/charts/${submodule}
 done
-helm repo index cp4mcm-install/charts
+helm repo index multicloudhub/charts
