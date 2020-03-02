45cc2b5d4d2405faeab385e5deb8a4c1a19c3dd2
Connor Wyckoff <connor.wyckoff@ibm.com>
2020-03-02 14:07:00 -0500

    xargs -j

--- cicd-scripts/chart-sync.sh
+++ cicd-scripts/chart-sync.sh
@@ -6,13 +6,13 @@ do
   cd tmp
   git clone $f1
   cd */
-  git checkout $f2
+  #git checkout $f2
   var1=$(echo $f1 | cut -f5 -d/)  #get the repo name
   var2=$(echo $var1 | cut -f1 -d-) #get the first word (ie kui in kui-web-terminal)
   var3=$(find . -type d -name "$var2*") #look for folder in repo that starts with ^ rather than stable/*/
   cd $var3
   PACKAGE="$(helm package ./)"
-  find . -name '*tgz' | xargs -J% mv % $CHARTS_PATH
+  find . -type f -name "*tgz" | xargs -I '{}' mv '{}' ../../../../../temp
   cd ../../../../
   rm -rf tmp
 done < chartSHA.csv
