7172ce5f3b0125c33089c3d0756625a295c6e7df
Connor Wyckoff <connor.wyckoff@ibm.com>
2020-03-09 14:33:33 -0400

    accept ours only for chart changes

--- cicd-scripts/build.sh
+++ cicd-scripts/build.sh
@@ -5,7 +5,7 @@ cd $(dirname $0)
 
 git add ../multicloudhub/charts
 git commit -m "[skip ci] skip travis"
-git pull origin "HEAD:${TRAVIS_BRANCH}" -s recursive -X ours
+git pull origin master -s recursive -X ours --../multicloudhub/charts
 git push origin "HEAD:${TRAVIS_BRANCH}"
 
 cd ..
