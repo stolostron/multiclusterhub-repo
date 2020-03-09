2ebede8bfbaab38fde55873558f28e590c5d1326
Connor Wyckoff <connor.wyckoff@ibm.com>
2020-03-09 11:22:04 -0400

    move pull

--- cicd-scripts/build.sh
+++ cicd-scripts/build.sh
@@ -3,9 +3,9 @@
 cd $(dirname $0)
 . chart-sync.sh
 
-git pull origin "HEAD:${TRAVIS_BRANCH}" -s recursive -X ours
 git add ../multicloudhub/charts
 git commit -m "[skip ci] skip travis"
+git pull origin "HEAD:${TRAVIS_BRANCH}" -s recursive -X ours
 git push origin "HEAD:${TRAVIS_BRANCH}"
 
 cd ..
