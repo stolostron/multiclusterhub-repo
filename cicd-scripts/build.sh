a76816cc3c42766c4a1b7f71f2b3e5443cf32562
Connor Wyckoff <connor.wyckoff@ibm.com>
2020-03-10 11:52:46 -0400

    allow history

--- cicd-scripts/build.sh
+++ cicd-scripts/build.sh
@@ -3,10 +3,10 @@
 cd $(dirname $0)
 . chart-sync.sh
 
-if [ $TRAVIS_BRANCH != "master" ] || [ $TRAVIS_BRANCH != "release-1.0.0" ]; then 
+if [ $TRAVIS_BRANCH != "master" ] && [ $TRAVIS_BRANCH != "release-1.0.0" ]; then 
     git add ../multicloudhub/charts
     git commit -m "[skip ci] skip travis"
-    git pull origin master -s recursive -X ours
+    git pull origin master -s recursive -X ours --allow-unrelated-histories
     git push origin "HEAD:${TRAVIS_BRANCH}"
 fi
 
