8fd7035e5627800fb934b5d27d3278bcef474f05
Connor Wyckoff <connor.wyckoff@ibm.com>
2020-03-11 11:08:23 -0400

    dont index on master merge

--- cicd-scripts/build.sh
+++ cicd-scripts/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd $(dirname $0)
-. chart-sync.sh
 
 if [ $TRAVIS_BRANCH != "master" ] && [ $TRAVIS_BRANCH != "release-1.0.0" ]; then 
+    . chart-sync.sh
     git add ../multicloudhub/charts
     git commit -m "[skip ci] skip travis"
     git pull origin master -s recursive -X ours --allow-unrelated-histories
