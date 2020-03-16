95f374030ce5bff135cd76f55d2dbacacc540d4e
Connor Wyckoff <connor.wyckoff@ibm.com>
2020-03-16 14:32:45 -0400

    [skip ci] Comment out

--- cicd-scripts/build.sh
+++ cicd-scripts/build.sh
@@ -2,11 +2,11 @@
 
 cd $(dirname $0)
 
-. chart-sync.sh
-git add ../multiclusterhub/charts
-git commit -m "[skip ci] skip travis"
-git pull origin master -s recursive -X ours --allow-unrelated-histories
-git push origin "HEAD:${TRAVIS_BRANCH}"
+# . chart-sync.sh
+# git add ../multiclusterhub/charts
+# git commit -m "[skip ci] skip travis"
+# git pull origin master -s recursive -X ours --allow-unrelated-histories
+# git push origin "HEAD:${TRAVIS_BRANCH}"
 
 # cd ..
 docker build -t $1 .
