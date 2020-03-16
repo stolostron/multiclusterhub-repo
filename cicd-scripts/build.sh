9cbc384d5aaf2511075cd1b8943a2222b482671f
Connor Wyckoff <connor.wyckoff@ibm.com>
2020-03-16 18:03:37 -0400

    new automation

--- cicd-scripts/build.sh
+++ cicd-scripts/build.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
 cd $(dirname $0)
+echo "TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
 
+# if we are merging into master, update the charts and 
+# amend the charts to the previous commit
+if [ "${TRAVIS_BRANCH}" == "chartTest" ]; then
+    git checkout "${TRAVIS_BRANCH}"
+    . chart-sync.sh
+    git add ../multiclusterhub/charts
+    git commit --amend --no-edit
+    git push origin +${TRAVIS_BRANCH}:${TRAVIS_BRANCH}
+
+    #now merge the new master branch into release-1.0.0
+
+    git checkout chartAutomation
+    git merge chartTest 
+fi
 # . chart-sync.sh
 # git add ../multiclusterhub/charts
 # git commit -m "[skip ci] skip travis"
 # git pull origin master -s recursive -X ours --allow-unrelated-histories
 # git push origin "HEAD:${TRAVIS_BRANCH}"
 
-# cd ..
+cd ..
 docker build -t $1 .
-
-##
