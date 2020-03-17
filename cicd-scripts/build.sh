01e13479450e2279aafea8c2bbd782213c0172b9
Connor Wyckoff <connor.wyckoff@ibm.com>
2020-03-17 16:48:20 -0400

    clone first

--- cicd-scripts/build.sh
+++ cicd-scripts/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 cd $(dirname $0)
 
 . chart-sync.sh
@@ -8,6 +7,10 @@ cd ..
 docker build -t $1 .
 
 if [ "${TRAVIS_BRANCH}" != "master" ] && [ "${TRAVIS_BRANCH}" != "release-1.0.0" ]; then
+    git clone git@github.com:open-cluster-management/multicloudhub-repo.git
+    cd multicloudhub-repo
+    git checkout "${TRAVIS_BRANCH}"
+    . chart-sync.sh
     git add .
     git merge master -m "[skip ci] resolve conflicts" -s recursive -X ours
     git push origin "HEAD:${TRAVIS_BRANCH}"
