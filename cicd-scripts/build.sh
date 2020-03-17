bdb77aecd686cf80f92de802871879b39632d399
Connor Wyckoff <connor.wyckoff@ibm.com>
2020-03-17 16:39:43 -0400

    merge

--- cicd-scripts/build.sh
+++ cicd-scripts/build.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-# cd $(dirname $0)
+cd $(dirname $0)
 
-# . chart-sync.sh
+. chart-sync.sh
+
+cd ..
+docker build -t $1 .
+
+if [ "${TRAVIS_BRANCH}" != "master" ] && [ "${TRAVIS_BRANCH}" != "release-1.0.0" ]; then
+    git add .
+    git merge master -m "[skip ci] resolve conflicts" -s recursive -X theirs
+    git push origin "HEAD:${TRAVIS_BRANCH}"
+fi
+
+
 # git add ../multiclusterhub/charts
 # git commit -m "[skip ci] skip travis"
 # git pull origin master -s recursive -X ours --allow-unrelated-histories
 # git push origin "HEAD:${TRAVIS_BRANCH}"
-
-# cd ..
-docker build -t $1 .
