ecc3b2f3e30b8a15ceab517cc081ac6386ece731
Connor Wyckoff <connor.wyckoff@ibm.com>
2020-03-17 16:52:48 -0400

    correct script path

--- cicd-scripts/build.sh
+++ cicd-scripts/build.sh
@@ -10,7 +10,7 @@ if [ "${TRAVIS_BRANCH}" != "master" ] && [ "${TRAVIS_BRANCH}" != "release-1.0.0"
     git clone git@github.com:open-cluster-management/multicloudhub-repo.git
     cd multicloudhub-repo
     git checkout "${TRAVIS_BRANCH}"
-    . chart-sync.sh
+    cicd-scripts/chart-sync.sh
     git add .
     git merge master -m "[skip ci] resolve conflicts" -s recursive -X ours
     git push origin "HEAD:${TRAVIS_BRANCH}"
