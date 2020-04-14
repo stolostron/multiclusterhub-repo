5cc457ded2ddbffde4a7567e17577523e26dd8fd
Jakob Gray <jdg7sh@virginia.edu>
2020-04-14 12:21:26 -0400

    Omit build harness from sonar coverage scan; Don't commit charts on dev- branches

--- cicd-scripts/build.sh
+++ cicd-scripts/build.sh
@@ -2,7 +2,7 @@
 cd $(dirname $0)
 
 #if on PR not master or release, update PR with latest charts.Otherwise just build image
-if [ "${TRAVIS_BRANCH}" != "master" ] && [[ "${TRAVIS_BRANCH}" != "release-"* ]]; then
+if [ "${TRAVIS_BRANCH}" != "master" ] && [[ "${TRAVIS_BRANCH}" != "release-"* ]] && [[ "${TRAVIS_BRANCH}" != "dev-"* ]]; then
     git clone git@github.com:open-cluster-management/multicloudhub-repo.git
     cd multicloudhub-repo
     git checkout "${TRAVIS_BRANCH}"
