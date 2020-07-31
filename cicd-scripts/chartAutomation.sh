e2c037014afae1f12f16fb72a920d30c00c827f8
Connor Wyckoff <connorwyckoff@comcast.net>
2020-07-31 13:38:17 -0400

    fake try from own repo

---
+++ cicd-scripts/chartAutomation.sh
@@ -0,0 +1,13 @@
+# Copyright (c) 2020 Red Hat, Inc.
+
+#Script to be run in travis when charts are updated in master
+
+
+cd $(dirname $0)
+git clone https://github.com/open-cluster-management/multicloudhub-repo.git
+cd multicloudhub-repo
+git checkout chartAutomation
+cicd-scripts/chart-sync.sh
+git add .
+git commit -m "[skip ci] greetings from fakeMaster"
+git push  https://github.com/open-cluster-management/multicloudhub-repo.git "chartAutomation"
