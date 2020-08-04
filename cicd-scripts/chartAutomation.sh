9066eef75a992bbfe7be2f7e1c6dfbf6c18ff194
Connor Wyckoff <connorwyckoff@comcast.net>
2020-08-04 15:06:38 -0400

    [skip ci] https

--- cicd-scripts/chartAutomation.sh
+++ cicd-scripts/chartAutomation.sh
@@ -6,4 +6,4 @@ cicd-scripts/install-dependencies.sh
 cicd-scripts/chart-sync.sh
 git add .
 git commit -m "Update from $1"
-git push  git@github.com:open-cluster-management/multicloudhub-repo.git "dev-auto"
+git push  https://${GITHUB_TOKEN}@github.com/open-cluster-management/multicloudhub-repo.git "dev-auto"
