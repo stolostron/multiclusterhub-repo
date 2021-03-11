5196701125ea4cbc5db85d6cebd752d6c8cba23d
Zachary Kayyali <zkayyali@redhat.com>
2021-03-11 16:43:08 -0500

    Fix chart automation - release-2.0

--- cicd-scripts/build.sh
+++ cicd-scripts/build.sh
@@ -5,14 +5,14 @@ cd $(dirname $0)
 
 #if on PR not master or release, update PR with latest charts.Otherwise just build image
 if [ "${TRAVIS_BRANCH}" != "master" ] && [[ "${TRAVIS_BRANCH}" != "release-"* ]] && [[ "${TRAVIS_BRANCH}" != "dev-"* ]]; then
-    git clone git@github.com:open-cluster-management/multicloudhub-repo.git
+    git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/open-cluster-management/multicloudhub-repo.git
     cd multicloudhub-repo
     git checkout "${TRAVIS_BRANCH}"
     cicd-scripts/chart-sync.sh
     docker build -t $1 .
     git add .
     git commit -m "[skip ci] add charts"
-    git merge release-2.0 -m "[skip ci] resolve conflicts" -s recursive -X ours
+    git merge origin/release-2.0 -m "[skip ci] resolve conflicts" -s recursive -X ours
     git push origin "HEAD:${TRAVIS_BRANCH}"
 else 
     cd ..
