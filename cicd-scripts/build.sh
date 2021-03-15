48f9ba4398b5d53bdda89e1781f09a24caa63bca
Zachary Kayyali <zkayyali@redhat.com>
2021-03-15 12:13:45 -0400

    remove [skip-ci]
    Signed-off-by: Zachary Kayyali <zkayyali@redhat.com>

--- cicd-scripts/build.sh
+++ cicd-scripts/build.sh
@@ -8,11 +8,20 @@ if [ "${TRAVIS_BRANCH}" != "master" ] && [[ "${TRAVIS_BRANCH}" != "release-"* ]]
     git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/open-cluster-management/multicloudhub-repo.git
     cd multicloudhub-repo
     git checkout "${TRAVIS_BRANCH}"
+    git pull origin "${TRAVIS_BRANCH}"
+    lastCommitMsg=$(git log -1 --pretty=%B)
+
+    if [[ "$lastCommitMsg" == *"[skip-chart-sync]"* ]]; then
+        echo "Chart rebuild unnecessary charts due to [skip-chart-sync] commit msg."
+        docker build -t $1 .
+        exit 0
+    fi
+
     cicd-scripts/chart-sync.sh
     docker build -t $1 .
     git add .
-    git commit -m "[skip ci] add charts"
-    git merge origin/release-2.2 -m "[skip ci] resolve conflicts" -s recursive -X ours
+    git commit -m "[skip-chart-sync] add charts"
+    git merge origin/release-2.2 -m "[skip-chart-sync] resolve conflicts" -s recursive -X ours
     git push origin "HEAD:${TRAVIS_BRANCH}"
 else 
     cd ..
