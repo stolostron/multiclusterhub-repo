fe955750e8abc93b77214abadfc7ce24fd5cc8ef
Jakob Gray <20209054+JakobGray@users.noreply.github.com>
2021-03-16 18:14:01 -0400

    Remove unneeded CICD files

--- cicd-scripts/install-dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2020 Red Hat, Inc.
-# Copyright Contributors to the Open Cluster Management project
-
-echo "INSTALL DEPENDENCIES GOES HERE!"
-
-curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash 
