4b46de22532779d19082a0a4bbfd247e5fd8f79b
Nathan Weatherly <nweather@redhat.com>
2021-06-24 09:28:24 -0400

    adding dco support to 2.1 automation
    Signed-off-by: Nathan Weatherly <nweather@redhat.com>

--- cicd-scripts/chartAutomation.sh
+++ cicd-scripts/chartAutomation.sh
@@ -5,5 +5,5 @@
 cicd-scripts/install-dependencies.sh
 cicd-scripts/chart-sync.sh
 git add .
-git commit -m "Update from $1"
-git push https://${MCH_REPO_BOT_TOKEN}@github.com/open-cluster-management/multiclusterhub-repo.git "master"
+git commit -s -m "Update from $1"
+git push https://${MCH_REPO_BOT_TOKEN}@github.com/open-cluster-management/multiclusterhub-repo.git "release-2.1"
